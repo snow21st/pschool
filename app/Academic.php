48f817e11152b13164f3bff7a1b3d1293ef767a8
bobo <bobo.im2@gmail.com>
2020-09-01 13:07:25 +0630

    1st commit

---
+++ app/Academic.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Academic extends Model
+{
+     protected $fillable = [
+      'year','startdate','enddate'
+    ];
+}
---
+++ app/Classroom.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Classroom extends Model
+{
+     protected $fillable = [
+      'name','grade_id'
+    ];
+}
